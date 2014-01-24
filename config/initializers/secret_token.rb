eb239a9d9120bb9d9d0727672d7d66e94e45146f
Mainier <michaelmainier@new-host-2.home>
2014-01-23 19:55:11 -0500

    Create emilysite application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emilysite::Application.config.secret_key_base = '9c658124d8c74b44efff08a521a2ea1025b9c29cbba1387c7d8a3cd5566aa24ba649668e6ab22ade9ae5bd13885d97c6748dbc88448774d54e593439d09f9b84'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Emilysite::Application.config.session_store :cookie_store, key: '_emilysite_session'
